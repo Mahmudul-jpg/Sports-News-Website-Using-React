43015244d4fb674dcae5265ec84c38392f231c17
Mahmudul Hasan <shuvonovo@gmail.com>
2021-09-16 22:57:24 +0600

    sports news website using react

---
+++ src/components/SlidebarData.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import * as IoIcons from 'react-icons/io'
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        className: 'nav-text'
+    },
+    {
+        title: 'About Us',
+        path: '/about',
+        icon: <IoIcons.IoIosPaper />,
+        className: 'nav-text'
+    },
+]
+
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>whats up</h1>
+        </div>
+    )
+}
