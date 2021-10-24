29265c342dd2835899dd02745e045df4d630e668
Mahmudul Hasan <shuvonovo@gmail.com>
2021-10-24 20:25:22 +0600

    added functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+//using class component
 import React from 'react'
 // import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import { AiTwotoneRightSquare } from 'react-icons/ai'
+// import { AiTwotoneRightSquare } from 'react-icons/ai'
 import Headline from './Headline/Headline'
 
 
@@ -85,3 +85,63 @@ class App extends React.Component {
 }
 export default App
 
+//////////   Bootstrap
+//using functional component
+// import React, { useEffect, useState } from 'react'
+// // import './App.css';
+// import Navbar from './components/Navbar/Navbar'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import Home from './pages/Home'
+// import About from './pages/About'
+// import Headline from './Headline/Headline'
+
+
+// const App = () => {
+//   const [result, setResult] = useState({});
+//   const [isLoading, setIsLoading] = useState(false);
+
+
+//   useEffect(() => {
+//     fetch('https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=61ebfa3a3e6446e1bdbda8db38520ba4')
+//       .then(res => res.json())
+//       .then(data => {
+//         setResult(data)
+//         setIsLoading(true)
+//       });
+
+//   }, [])
+
+
+//   if (isLoading) {
+//     return (
+//       <div>
+//         <BrowserRouter>
+//           <Navbar />
+//           <Switch>
+//             <Route path="/home" exact component={Home} />
+//             <Route path="/about" exact component={About} />
+//             <div>
+//               {result.articles.map((item, index) => {
+//                 return <Headline title={item.title}
+//                   description={item.description}
+//                   image={item.urlToImage}
+//                 />
+//               })}
+//             </div>
+//           </Switch>
+//         </BrowserRouter>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <BrowserRouter>
+//         <Navbar />
+//         <Switch>
+//           <Route path="/" exact component={Home} />
+//           <Route path="/about" exact component={About} />
+//         </Switch>
+//       </BrowserRouter>
+//     );
+//   }
+// }
+// export default App;
